679127d8f9ecbbace4fcde85d4aa8a5462e132c9
Ubuntu <ubuntu@ip-172-31-37-161.ec2.internal>
2025-10-27 18:31:00 +0000

    still failing

--- scan/scan.cu
+++ scan/scan.cu
@@ -116,7 +116,7 @@ void exclusive_scan(int* input, int N, int* result)
     // to CUDA kernel functions (that you must write) to implement the
     // scan.
     N = nextPow2(N); // round N to next power of 2
-    int threads_per_block;
+    // int threads_per_block;
     // if (N < 4194304)
     //     threads_per_block = 256;
     // else
@@ -125,7 +125,7 @@ void exclusive_scan(int* input, int N, int* result)
     for (int two_d = 1; two_d <= N/2; two_d *= 2) {
         int two_dplus1 = two_d * 2;
         // launch upsweep kernel
-        int blocks = ((N / two_dplus1)  / threads_per_block) + 1;
+        int blocks = ((N / two_dplus1)  / THREADS_PER_BLOCK) + 1;
         upsweep_kernel<<<blocks, THREADS_PER_BLOCK>>>(N, two_d, two_dplus1, result);
         cudaDeviceSynchronize();
         // dump_device_int_array("up", result, N, two_d);
@@ -137,7 +137,7 @@ void exclusive_scan(int* input, int N, int* result)
     // downsweep phase
     for (int two_d = N/2; two_d >= 1; two_d /= 2) {
         int two_dplus1 = 2*two_d;
-        int blocks = ((N / two_dplus1) / threads_per_block) + 1;
+        int blocks = ((N / two_dplus1) / THREADS_PER_BLOCK) + 1;
         downsweep_add<<<blocks, THREADS_PER_BLOCK>>>(N, two_d, two_dplus1, result);
         dump_device_int_array("d1", result, N, two_d);
         cudaDeviceSynchronize();
